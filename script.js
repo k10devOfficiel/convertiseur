be866a2af39972e29c6b0542a72b4acae4260600
k10dev <kouamedieudonne93@gmail.com>
2026-01-22 20:45:02 +0000

    version ameliorée

--- script.js
+++ script.js
@@ -15,20 +15,29 @@ let form= document.querySelector('form')
 let inp =document.querySelector('input[type="text"]')
 
  function verifie(){
+ const resultat = document.querySelector('.resultat');
+const valueBasse = 650.50
+  if (!inp.value.trim()) {
+        resultat.textContent = "erreur de saisie";
+        return;
+    }
+
+    if (isNaN(recupInput)) {
+        resultat.textContent = "Veuillez entrer un nombre valide";
+        return;
+    }
     
     if (vm1.checked && Ce2.checked) {
-        const valueBasse = 650.50
        let  recupInput =Number(inp.value.trim());
        let resultas = recupInput/valueBasse;
-        let  resultat= document.querySelector('.resultat')
-         resultat.textContent=resultas + "" +"EURO"
+       let conver=resultas.toFixed(2)
+         resultat.textContent=conver + " " +"EURO"
        
     } else if (vm2.checked && Ce1.checked) {
-        const valueBasse = 650.50
         let  recupInput =Number(inp.value.trim());
         let resultas = recupInput*valueBasse;
-        let  resultat= document.querySelector('.resultat')
-         resultat.textContent=resultas + "" +"FRCFA"
+         let conver=resultas.toFixed(2)
+         resultat.textContent=conver + " " +"FRCFA"
     } else if (vm1.checked  && Ce1.checked) {
         let  recupInput =Number(inp.value.trim());
         let  resultat= document.querySelector('.resultat')
